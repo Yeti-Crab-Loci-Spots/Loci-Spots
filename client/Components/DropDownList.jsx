405848565edadef3dd895a14346f3e7084cc806d
dnagpal1 <105174832+dnagpal1@users.noreply.github.com>
2022-09-03 11:12:39 -0400

    initial front end commit

---
+++ client/Components/DropDownList.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import {Select} from '@mui/material';
+const DropDownList = (props) => {
+  const { citySearch:city } = props;
+
+  return (
+    <div>DropDownList</div>
+  )
+}
+
+export default DropDownList
---
+++ client/Components/MainContainer.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const MainContainer = () => {
+    //main container will store the state of the drop down list and the state of the restaurant container which contains a list of the restaurants
+  return (
+    <div>
+    <div>MainContainer</div>
+    <div className="citySearch"></div>
+    <div className="restaurantContainer"></div>
+    </div>
+
+  )
+}
+
+export default MainContainer
