668b57d69fae54cc603f582f5ba66f4aad16b155
ramabdel12 <77136023+ramabdel12@users.noreply.github.com>
2022-09-08 17:09:43 +0200

    added functionality

--- client/Components/LoginPage.jsx
+++ client/Components/LoginPage.jsx
@@ -6,21 +6,42 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const LoginPage = () =>{
 
-    const [user, setUser] = useState({name:'', username:''});
+    const [user, setUser] = useState({username:'', password:''});
     const [error, setError] = useState('');
 
-    const login = (details) => {
-        console.log(details);
+    // window.sessionStorage.setItem('isLoggedIn', false);
+
+    const login = async (details) => {
+        console.log('details in LoginPage', details);
         //see if username and password combo are in the database
         //if(axios.get('/username') ?)
+        const {username, password } = details;
+
+        console.log('USERNAME', username);
+        console.log('PASSWORD', password);
+
+        console.log('USER before setUser', user) //user is empty strings here
+
         setUser({
-            name: details.name,
-            username: details.username
+            username: username,
+            password: password
         })
+        
+        const result = await fetch('/api/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        });
+
+        const parsedResult = await result.json();
+        console.log('parsed result',parsedResult)
+        if(result.status === 200){
+            sessionStorage.setItem('isLoggedIn', true);
+            sessionStorage.setItem('name', parsedResult);
+        }
     }
-
-   
-
 
 
     return (
